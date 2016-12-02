4fe670d67ad156787cfaf6ef9a5f9013f28a6a24
Ryan P.C. McQuen <ryan.q@linux.com>
2016-12-01 20:38:55 -0800

    Create 01_algs__bubble_sort.py

---
+++ 01_algs__bubble_sort.py
@@ -0,0 +1,33 @@
+##
+## Algorithms Lesson 1: Bubblesort
+## https://www.youtube.com/watch?v=P00xJgWzz2c
+##
+
+##
+## Comparisons =
+##     (n - 1) + (n - 2) + ... + 1
+##     n(n - 1)/2
+##     n^2/2 - n/2
+##     O(n^2)
+##
+
+## Original pseudocode:
+'''
+for i = (n - 1) to 1
+    for j = 0 to (i - 1)
+        if A[j] > A[j + 1]
+            swap(A[j], A[j + 1])
+'''
+
+A = [5, 9, 2, 7, 1]
+n = len(A)
+
+for i in range(n, 1, -1):
+    for j in range(0, i - 1):
+        if (A[j] > A[j + 1]):
+            # Swap:
+            A[j], A[j + 1] = A[j + 1], A[j]
+
+print(A)
+
+## => [1, 2, 5, 7, 9]
